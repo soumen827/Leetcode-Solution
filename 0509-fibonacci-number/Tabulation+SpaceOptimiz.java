dfe51164828b3a20b29dc9ded511467256b4aaf2
Soumen Laha <soumenlaha465@gmail.com>
2026-08-09 10:22:47 +0530

    Create Tabulation+SpaceOptimiz.java

---
+++ 0509-fibonacci-number/Tabulation+SpaceOptimiz.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1) return n;
+        int[] dp = new int[3];
+        dp [0] =0;dp[1] =1;
+        for(int i=2;i<=n;i++){
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
+
+    }
+}
