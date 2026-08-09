a771164f6a366307fa06e618c2794cd41209797b
Soumen Laha <soumenlaha465@gmail.com>
2026-08-09 16:49:48 +0530

    Create Memoization.java

---
+++ 0279-perfect-squares/Memoization.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPerfect(int n) {
+        int sqrt = (int)(Math.sqrt(n));
+        return (sqrt*sqrt==n); //rott ka squar us no ho gaya to perf square
+    }
+    public int minSquares(int n,int [] dp) {
+        if(isPerfect(n)) return 1; //base
+        if(dp[n]!=-1) return dp[n];
+        int min = Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){ // i<=sqrt(n) --> i^2<=n
+            int count = minSquares(i*i,dp) + minSquares(n-i*i,dp);
+            min = Math.min(count,min);
+        }
+        return dp[n]=  min;
+    }
+    public int numSquares(int n) {
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return minSquares(n,dp);
+    }
+}
