9cb8adf6641183c1ba0086144466cd880a08b1e1
Soumen Laha <soumenlaha465@gmail.com>
2026-08-09 10:22:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,14 @@
 class Solution {
-    //static int[] dp;
     public int fib(int n) {
-        int[] dp =new int[n+1];
-        return fibo(n,dp);
-    }
-    public int fibo(int n,int[] dp){
         if(n<=1) return n;
-        if(dp[n]!=0) return dp[n]; // dp me kuch hay to ohi ans hay
-        int ans = fibo(n-1,dp)+ fibo(n-2,dp);
-        dp[n] = ans; // ans ko dp me save karo
-        return ans;
+        int[] dp = new int[3];
+        dp [0] =0;dp[1] =1;
+        for(int i=2;i<=n;i++){
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
+
     }
 }
