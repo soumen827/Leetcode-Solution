3dc59450576b160ff6853e66487fb3b03582f454
Soumen Laha <soumenlaha465@gmail.com>
2026-08-17 19:53:07 +0530

    Time: 81 ms (31.3%), Space: 72.7 MB (28.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,31 +5,20 @@ class Solution {
         if(sum%2 !=0) return false;
         int target = sum/2;
         int n = nums.length;
-        int[][] dp = new int[n+1][target+1];
+        int[][] dp = new int[n][target+1];
         for(int i=0;i<dp.length;i++){
            for(int j=0;j<dp[0].length;j++){
-            dp[i][j] =-1;
+                boolean ans = false; 
+                boolean skip = (i>0) ? (dp[i-1][j]==1) : (j==0);
+                if(j-nums[i]<0) ans = skip;
+                else{
+                boolean take = (i>0) ? (dp[i-1][j-nums[i]]==1):(j==0);
+                ans = take || skip;
+                }
+                dp[i][j] = (ans) ? 1 : 0;
            }
         }
          
-       return subset(0,nums,target,dp);
+       return (dp[n-1][target]==1);
     }
-    private boolean subset(int i,int[] arr, int target,int[][]dp) {
-        int n = arr.length;
-        if(i==n){ // last tak agaya
-            if(target==0) return true;
-            else return false;
-        }
-        boolean ans = false; //extra
-        if(dp[i][target]!= -1) return (dp[i][target]==1);// dp int ka hay and return type bool
-        boolean skip = subset(i+1,arr,target,dp);
-        if(target-arr[i]<0) ans = skip;
-        else{
-           boolean take = subset(i+1,arr,target-arr[i],dp);//take matlab sum+ us arry idx
-           ans = take || skip;
-        }
-        if(ans==true) dp[i][target] = 1;
-        else  dp[i][target] =0;
-        return ans;
-    }
-}
+}    
