e2f5b8d21f7a0b4b9f820f739c7b2230baf0178b
Soumen Laha <soumenlaha465@gmail.com>
2026-08-17 21:15:55 +0530

    Time: 25 ms (32.41%), Space: 47.3 MB (17.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,23 +4,16 @@ class Solution {
         long[][]dp = new long [n][amount+1];
         for(int i=0;i<dp.length;i++){
             for(int j=0; j<dp[0].length;j++){
-                dp[i][j] = -1;
+        long skip = (i>0) ? dp[i-1][j]: ((j==0) ? 0:Integer.MAX_VALUE);
+                if(j-coins[i]<0)  dp[i][j] = skip;
+                else{
+        long take = 1 + dp[i][j-coins[i]];
+                    dp[i][j] = Math.min(skip,take);
+                }
             }
         }
-        int ans =(int)noOfCoin(n-1,coins,amount,dp); //convert long to int
-        if(ans==Integer.MAX_VALUE) return -1; // for the test case  max value aya to -1 retuen 
-        else return ans;
-         
-    }
-    public long noOfCoin(int i, int[] coins, int amount,long[][]dp) {
-        if(i==-1) {
-            if(amount==0) return 0;// no more coin needed
-            else return Integer.MAX_VALUE;// not a valid combination not return 1
-        }
-        if(dp[i][amount] != -1) return (dp[i][amount]);
-        long skip = noOfCoin(i-1,coins,amount,dp);
-        if(amount-coins[i]<0) return dp[i][amount] = skip;
-        long take = 1 + noOfCoin(i,coins,amount-coins[i],dp);
-        return dp[i][amount] = Math.min(skip,take);
+        int ans =(int)dp[n-1][amount];
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
     }
 }
