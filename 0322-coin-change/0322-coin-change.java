2a6093af99131bd88d0cd425c342dbc1e7aabbaa
Soumen Laha <soumenlaha465@gmail.com>
2026-08-17 22:19:46 +0530

    Time: 27 ms (27.79%), Space: 46.7 MB (38.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,21 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        long[][]dp = new long [n][amount+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0; j<dp[0].length;j++){
-        long skip = (i>0) ? dp[i-1][j]: ((j==0) ? 0:Integer.MAX_VALUE);
-                if(j-coins[i]<0)  dp[i][j] = skip;
+        long[][]dp = new long [2][amount+1]; // only 2 row 
+        for(int i=0;i<n;i++){
+            for(int j=0; j<=amount;j++){
+        long skip = (i>0) ? dp[0][j]: ((j==0) ? 0:Integer.MAX_VALUE);
+                if(j-coins[i]<0)  dp[1][j] = skip;
                 else{
-        long take = 1 + dp[i][j-coins[i]];
-                    dp[i][j] = Math.min(skip,take);
+        long take = 1 + dp[1][j-coins[i]];
+                    dp[1][j] = Math.min(skip,take);
                 }
             }
+            for(int j=0;j<=amount;j++){ // copy pasing
+                dp[0][j] = dp[1][j];
+            }
         }
-        int ans =(int)dp[n-1][amount];
+        int ans =(int)dp[1][amount];
         if(ans==Integer.MAX_VALUE) return -1;
         return ans;
     }
