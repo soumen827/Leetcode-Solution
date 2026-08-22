89fd9e4dd44d8473cb81e75f2328499845390405
Soumen Laha <soumenlaha465@gmail.com>
2026-08-22 19:32:57 +0530

    Time: 37 ms (5.42%), Space: 53.7 MB (81.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,19 +4,25 @@ class Solution {
         StringBuilder b = new StringBuilder(text2);
         int m = a.length(), n = b.length();
         int[][] dp = new int[m][n]; // dp
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j] = -1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int p = (i>=1 && j>=1)? dp[i-1][j-1] : 0;
+                int r = (i>=1)? dp[i-1][j] : 0;
+                int q = (j>=1)? dp[i][j-1] : 0;
+                if(a.charAt(i)==b.charAt(j))
+                    dp[i][j] = 1+p;
+                else dp[i][j] = Math.max(q,r);
+
             }
         }
-       return lcs(m-1,n-1,a,b,dp);
+       return dp[m-1][n-1];
         
     }
-    public int lcs(int i, int j,StringBuilder a, StringBuilder b,int[][]dp) {
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return  dp[i][j];
-        if(a.charAt(i)==b.charAt(j)) return 1+ lcs(i-1,j-1,a,b,dp);
-        else return dp[i][j] = Math.max(lcs(i-1,j,a,b,dp),lcs(i,j-1,a,b,dp)); // akbar i-- j same rakha , or akbar j-- and i same 
+    // public int lcs(int i, int j,StringBuilder a, StringBuilder b,int[][]dp) {
+    //     if(i<0 || j<0) return 0;
+    //     if(dp[i][j] != -1) return  dp[i][j];
+    //     if(a.charAt(i)==b.charAt(j)) return 1+ lcs(i-1,j-1,a,b,dp);
+    //     else return dp[i][j] = Math.max(lcs(i-1,j,a,b,dp),lcs(i,j-1,a,b,dp)); // akbar i-- j same rakha , or akbar j-- and i same 
         
-    }
+    // }
 }
