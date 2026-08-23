fd393fe487f794d50afef445da09259cac189f87
Soumen Laha <soumenlaha465@gmail.com>
2026-08-23 11:39:00 +0530

    Time: 22 ms (46.15%), Space: 54.1 MB (36.46%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,20 +1,24 @@
 class Solution {
     public int longestCommonSubsequence(String a, String b) {
         int m = a.length(), n = b.length();
-        int[][] dp = new int[2][n+1]; // dp
+        int[][] dp = new int[m+1][n+1]; // dp
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(a.charAt(i-1)==b.charAt(j-1)) dp[1][j] = 1+dp[1-1][j-1] ;
-                else dp[1][j] = Math.max(dp[1-1][j],dp[1][j-1]);
+                if(a.charAt(i-1)==b.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1] ;
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
 
             }
-            //copy path
-                for(int j=0;j<=n;j++)
-                    dp[0][j] = dp[1][j];
         }
-       return dp[1][n]; // store the lcs of substr(a,0,m-1)&(b,0,n-1);
+        StringBuilder str = new StringBuilder("");
+        int i = m, j =n;
+        while(i>=1 && j>=1){
+           if(a.charAt(i-1)==b.charAt(j-1)) str.append(a.charAt(i-1));
+           if(dp[i][j-1]>dp[i-1][j])  j--;
+           else i--;
+           
+        }
+        System.out.println(str.reverse()); // reverse the StringBuilder
+       return dp[m][n]; // store the lcs of substr(a,0,m-1)&(b,0,n-1);
         
     }
 }
-// S.C = O(n)
-// T.C = (m*n)
