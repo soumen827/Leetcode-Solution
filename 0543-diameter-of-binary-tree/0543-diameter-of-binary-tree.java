e3fb9d65ea5596966350d792c72e027fec88a5b3
Soumen Laha <soumenlaha465@gmail.com>
2026-08-27 01:16:08 +0530

    Time: 4 ms (13.01%), Space: 47.1 MB (27.59%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,33 +1,25 @@
 class Solution {
+    static Map<TreeNode,Integer> dp; // global dp 
     public int level(TreeNode root){
         if(root==null) return 0;
+        if(dp.containsKey(root)) return dp.get(root); // like dp[i][j] != -1 return dp[i][j];
         int leftLevels = level(root.left);
         int rightLevels = level(root.right);
-        return 1 + Math.max(leftLevels,rightLevels);
+        dp.put(root,1+Math.max(leftLevels,rightLevels));// like dp[i][j]me save karte the return karne se pehele,isame map me put kar raha hay
+        return dp.get(root);
     }
-    public int diameterOfBinaryTree(TreeNode root) {
+    public int diameter(TreeNode root) {
         if(root==null) return 0;
         int myDia = (level(root.left) + level(root.right)+1)-1;
-        int leftDia = diameterOfBinaryTree(root.left);
-        int rightDia = diameterOfBinaryTree(root.right);
+        int leftDia = diameter(root.left);
+        int rightDia = diameter(root.right);
         return Math.max(Math.max(myDia,leftDia),rightDia);
         
     }
-    // static int maxDia;
-    // public int levels(TreeNode root){
-    //     if(root==null) return 0;
-    //     int leftLevels = levels(root.left);
-    //     int rightLevels = levels(root.right);
-    //     int dia =  leftLevels + rightLevels;
-    //     maxDia = Math.max(dia,maxDia);
-
-    //     return 1 +Math.max(leftLevels,rightLevels);
+     public int diameterOfBinaryTree(TreeNode root) {
+        dp = new HashMap<>();
+        return diameter(root);
         
-    // }
-    // public int diameterOfBinaryTree(TreeNode root){
-    //     maxDia = 0;
-    //     levels(root);
-    //     return maxDia;
-
-    // }
+    }
+  
 }
