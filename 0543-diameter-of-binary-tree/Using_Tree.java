7b172a6d409497be79d873a9d602fa072969d1ab
Soumen Laha <soumenlaha465@gmail.com>
2026-08-27 00:52:07 +0530

    Implement diameter calculation for binary tree

---
+++ 0543-diameter-of-binary-tree/Using_Tree.java
@@ -0,0 +1,31 @@
+class Solution {
+    // public int level(TreeNode root){
+    //     if(root==null) return 0;
+    //     return 1 + Math.max(level(root.left),level(root.right));
+    // }
+    // public int diameterOfBinaryTree(TreeNode root) {
+    //     if(root==null) return 0;
+    //     int myDia = (level(root.left) + level(root.right)+1)-1;
+    //     int leftDia = diameterOfBinaryTree(root.left);
+    //     int rightDia = diameterOfBinaryTree(root.right);
+    //     return Math.max(Math.max(myDia,leftDia),rightDia);
+        
+    // }
+    static int maxDia;
+    public int levels(TreeNode root){
+        if(root==null) return 0;
+        int leftLevels = levels(root.left);
+        int rightLevels = levels(root.right);
+        int dia =  leftLevels + rightLevels;
+        maxDia = Math.max(dia,maxDia);
+
+        return 1 +Math.max(leftLevels,rightLevels);
+        
+    }
+    public int diameterOfBinaryTree(TreeNode root){
+         maxDia = 0;
+         levels(root);
+         return maxDia;
+
+    }
+}
