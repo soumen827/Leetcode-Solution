58b619cc0f281cfa0e01d7ac3eacd14f0f317d2a
Soumen Laha <soumenlaha465@gmail.com>
2026-08-27 01:20:35 +0530

    Create Memoization.java

---
+++ 0543-diameter-of-binary-tree/Memoization.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int level(TreeNode root, Map<TreeNode,Integer> dp){
+        if(root==null) return 0;
+        if(dp.containsKey(root)) return dp.get(root); // like dp[i][j] != -1 return dp[i][j];
+        int leftLevels = level(root.left,dp);
+        int rightLevels = level(root.right,dp);
+        dp.put(root,1+Math.max(leftLevels,rightLevels));// like dp[i][j]me save karte the return karne se pehele,isame map me put kar raha hay
+        return dp.get(root);
+    }
+    public int diameter(TreeNode root, Map<TreeNode,Integer> dp) {
+        if(root==null) return 0;
+        int myDia = (level(root.left,dp) + level(root.right,dp)+1)-1;
+        int leftDia = diameter(root.left,dp);
+        int rightDia = diameter(root.right,dp);
+        return Math.max(Math.max(myDia,leftDia),rightDia);
+        
+    }
+     public int diameterOfBinaryTree(TreeNode root) {
+       Map<TreeNode,Integer> dp = new HashMap<>();
+        return diameter(root,dp);
+        
+    }
+}
+// T.C = O(n)
