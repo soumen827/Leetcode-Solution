097ccb09f7d147fb95f51374bed6a2ddf72f10af
Soumen Laha <soumenlaha465@gmail.com>
2026-08-27 11:26:02 +0530

    Time: 3 ms (13.2%), Space: 47.4 MB (9.55%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,23 +1,48 @@
 class Solution {
-    public int level(TreeNode root, Map<TreeNode,Integer> dp){
+    // public int level(TreeNode root, Map<TreeNode,Integer> dp){
+    //     if(root==null) return 0;
+    //     if(dp.containsKey(root)) return dp.get(root); // like dp[i][j] != -1 return dp[i][j];
+    //     int leftLevels = level(root.left,dp);
+    //     int rightLevels = level(root.right,dp);
+    //     dp.put(root,1+Math.max(leftLevels,rightLevels));// like dp[i][j]me save karte the return karne se pehele,isame map me put kar raha hay
+    //     return dp.get(root);
+    // }
+    // public int diameter(TreeNode root, Map<TreeNode,Integer> dp) {
+    //     if(root==null) return 0;
+    //     int myDia = (level(root.left,dp) + level(root.right,dp)+1)-1;
+    //     int leftDia = diameter(root.left,dp);
+    //     int rightDia = diameter(root.right,dp);
+    //     return Math.max(Math.max(myDia,leftDia),rightDia);
+        
+    // }
+    //  public int diameterOfBinaryTree(TreeNode root) {
+    //    Map<TreeNode,Integer> dp = new HashMap<>();
+    //     return diameter(root,dp);
+        
+    // }
+
+
+
+    static Map<TreeNode,Integer> dp;
+    public int level(TreeNode root){
         if(root==null) return 0;
         if(dp.containsKey(root)) return dp.get(root); // like dp[i][j] != -1 return dp[i][j];
-        int leftLevels = level(root.left,dp);
-        int rightLevels = level(root.right,dp);
+        int leftLevels = level(root.left);
+        int rightLevels = level(root.right);
         dp.put(root,1+Math.max(leftLevels,rightLevels));// like dp[i][j]me save karte the return karne se pehele,isame map me put kar raha hay
         return dp.get(root);
     }
-    public int diameter(TreeNode root, Map<TreeNode,Integer> dp) {
+    public int diameter(TreeNode root) {
         if(root==null) return 0;
-        int myDia = (level(root.left,dp) + level(root.right,dp)+1)-1;
-        int leftDia = diameter(root.left,dp);
-        int rightDia = diameter(root.right,dp);
+        int myDia = (level(root.left) + level(root.right)+1)-1;
+        int leftDia = diameter(root.left);
+        int rightDia = diameter(root.right);
         return Math.max(Math.max(myDia,leftDia),rightDia);
         
     }
      public int diameterOfBinaryTree(TreeNode root) {
-       Map<TreeNode,Integer> dp = new HashMap<>();
-        return diameter(root,dp);
+        dp = new HashMap<>();
+        return diameter(root);
         
     }
 }
