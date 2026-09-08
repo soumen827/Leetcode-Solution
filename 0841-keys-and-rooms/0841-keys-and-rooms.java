7baf2f82df0dcae5b0d89e911d957d3201036bb5
Soumen Laha <soumenlaha465@gmail.com>
2026-09-08 19:26:06 +0530

    Time: 2 ms (48.04%), Space: 46.3 MB (45.28%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> adj) {
         int n = adj.size();
         boolean [] Visited = new boolean[n];
-        // Visited[0] = true;
         
         bfs(0,Visited,adj);
         for(int i=0;i<n;i++){
@@ -15,15 +14,15 @@ class Solution {
         Visited[0] = true;
         Queue<Integer> q = new LinkedList<>();
         q.add(i);
-         while(q.size()>0){
+        while(q.size()>0){
             int front = q.remove();
-            for( int ele : adj.get(front)){
+            for( int ele : adj.get(front)){ // us idx ka all ele
                 if(!Visited[ele]) {
                     Visited[ele] = true;
                     q.add(ele);
                 }
             }
 
-         }
+        }
     }
 }
