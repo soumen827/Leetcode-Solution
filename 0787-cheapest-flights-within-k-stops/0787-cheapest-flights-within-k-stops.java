c9e381410eb8183182b4acd0ae6a7e7636f2c820
Soumen Laha <soumenlaha465@gmail.com>
2026-09-26 20:08:57 +0530

    Time: 6 ms (84.08%), Space: 46.8 MB (18.14%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,58 @@
+class Solution {
+    public class Pair {
+        int node;
+        int cost;
+        Pair(int node, int cost ) {
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+    public class Triplet implements Comparable<Triplet>{
+        int node;
+        int cost;
+        int stops;
+
+        Triplet(int node, int cost,int stops ) {
+            this.node = node;
+            this.cost = cost;
+            this.stops = stops;
+        }
+        public int compareTo(Triplet T){
+            if(this.stops==T.stops) return this.cost - T.cost; 
+            return this.stops-T.stops;
+        }
+    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        // create adj list for directed graph 
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<Pair>());
+        for(int i=0;i<flights.length;i++) {
+            int from = flights[i][0], to = flights[i][1];
+            int price = flights[i][2];
+            adj.get(from).add(new Pair(to,price));
+        }
+        //apply dijkastra
+        int[] ans = new int[n];
+        Arrays.fill(ans,Integer.MAX_VALUE);
+        ans[src] =0;
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        pq.add(new Triplet(src,0,0));
+        while(pq.size()>0){
+            Triplet top = pq.remove();
+            int node = top.node,cost= top.cost, stops = top.stops;
+            //if(node==dst) return ans[node]; // ans a gaya
+            if(stops==k+1) continue; // nehi lunga 
+            for(Pair p:adj.get(node)){
+                int totalCost = cost + p.cost;
+                if(totalCost < ans[p.node]){
+                    ans[p.node] = totalCost;
+                    pq.add(new Triplet(p.node,totalCost,stops+1));
+                }
+            }
+
+        }
+        if(ans[dst]==Integer.MAX_VALUE) return -1;
+        return ans[dst];// dst ka corosponding cost
+
+    }
+}
